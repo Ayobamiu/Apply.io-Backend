56d0dc3bea5989e773328321a78ada466394b2cf
Ayobamiu <ayobamiu@gmail.com>
2020-10-07 08:09:51 +0100

    set allowed host

--- apply_to/settings.py
+++ apply_to/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://apply-dot-to.herokuapp.com']
 
 
 # Application definition
